e86dd0dc58b437adda1bfdf6716ead49e065d234
Adriano Allef Pereira <adrianopereira951@outlook.com>
2025-08-02 19:29:17 -0300

    Aula 3 - Adiciona exemplo de operadores de atribuição em JavaScript

---
+++ 2 -Módulo-1A-User_Experience/Desenvolvimento_Dinamico/3-Operadores_e_Operacoes/operadores_Atribuicao.js
@@ -0,0 +1,31 @@
+// Operador de atribuição básico (=)
+let a = 10; // Atribui o valor 10 à variável a
+console.log(`Valor inicial de a: ${a}`); // Exibe o valor de a
+
+// Operador de atribuição com adição (+=)
+a += 5; // Adiciona 5 ao valor atual de a
+console.log(` Após a += 5, valor de a: ${a}`);
+
+// Operador de atribuição com subtração (-=)
+a -= 3; // a = a - 3
+console.log(` Após a -= 3, valor de a: ${a}`);
+
+// Operador de atribuição com multiplicação (*=)
+a *= 2; // a = a * 2
+console.log(` Após a *= 2, valor de a: ${a}`);
+
+// Operador de atribuição com divisão (/=)
+a /= 4; // a = a / 4
+console.log(` Após a /= 4, valor de a: ${a}`);
+
+// Operador de atribuição com módulo (%=)
+a %= 4; // a = a % 4
+console.log(` Após a %= 3, valor de a: ${a}`);
+
+// Operador de atribuição de resto (%)
+a = a % 2
+console.log(` Após a %= 2, valor de a: ${a}`);
+
+// Operador de atribuição com exponenciação (**=)
+a **= 2; // a = a ** 2
+console.log(` Após a **= 2, valor de a: ${a}`);

479b692ba859c43d6088a03b6f15aa007f137ae9
Adriano Allef Pereira <adrianopereira951@outlook.com>
2025-08-28 15:32:43 -0300

    Adiciona exemplos de conversão de strings para minúsculas e maiúsculas, incluindo tratamento de caracteres não alfabéticos

---
+++ 2 -Módulo-1A-User_Experience/Desenvolvimento_Dinamico/5- Trabalhando_com_strings/nomes.js
@@ -0,0 +1,22 @@
+// Definindo uma string de exemplo
+let nome = "Ana Teixeira";
+
+// Usando o método tolowerCase() para converter a string para minúsculas
+let nomeMinusculo = nome.toLowerCase();
+console.log("Nome em minúsculas:", nomeMinusculo);
+
+// Usando o método toUpperCase() para converter a string para maiúsculas
+let nomeMaiusculo = nome.toUpperCase();
+console.log("Nome em maiúsculas:", nomeMaiusculo);
+
+// Outra string com caracteres não alfabéticos
+
+// Outra string com caracteres não alfabéticos
+let stringComSimbolos = "Ana Cristina, 415";
+let minusculoSimbolos = stringComSimbolos.toLowerCase();
+// Convertendo para minúsculas
+Console.log("String com símbolos em minúculas: " + minusculoSimbolos);
+
+// Convertendo para maiúsculas
+let maiusculoSimbolos = stringComSimbolos.toUpperCase(); 
+console.log("String com símbolos em maiúsculas:" + maiusculoSimbolos);
